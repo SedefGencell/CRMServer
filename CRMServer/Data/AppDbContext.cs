ded0bc37d96adccbb44d426444823624becf5759
Sedef Gencel <sedefgencel1903@gmail.com>
2025-03-27 18:35:21 +0300

    Add project files.

---
+++ CRMServer/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CRMServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CRMServer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+         public DbSet<Employee> Employees { get; set; }
+    }
+
+}
+   
+    
+
